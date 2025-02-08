fc09dc80ea4501dd318204c4580a41e440803cad
sanghyeon.kim <boh001g@gmail.com>
2025-02-08 21:13:00 +0900

    update

--- nvim/init.lua
+++ nvim/init.lua
@@ -1,6 +1,6 @@
-if vim.fn.has("nvim-0.9") == 0 then
+if vim.fn.has("nvim-0.10") == 0 then
 	vim.api.nvim_echo({
-		{ "Requires Neovim >= 0.9.0\n", "ErrorMsg" },
+		{ "Requires Neovim >= 0.10.0\n", "ErrorMsg" },
 		{ "Press any key to exit", "MoreMsg" },
 	}, true, {})
 	vim.fn.getchar()
